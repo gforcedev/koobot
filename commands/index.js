eb2a32d4c42d447f8b8155b7699ca128862ff1a7
Gforcedev <tecshotclark@gmail.com>
2021-06-11 11:07:25 +0100

    Add mobilefixtures command

--- commands/index.js
+++ commands/index.js
@@ -4,6 +4,7 @@ const StandingsCommand = require('./standings');
 const StatsCommand = require('./stats')
 const GenerateCommand = require('./generate');
 const ViewfixturesCommand = require('./viewfixtures.js');
+const MobileFixturesCommand = require('./mobilefixtures.js');
 const ReportCommand = require('./report.js')
 const ConfirmCommand = require('./confirm.js');
 const RemoveCommand = require('./remove.js');
@@ -17,6 +18,7 @@ module.exports = {
 	stats: new StatsCommand(),
 	generate: new GenerateCommand(),
 	viewfixtures: new ViewfixturesCommand(),
+	mobilefixtures: new MobileFixturesCommand(),
 	report: new ReportCommand(),
 	confirm: new ConfirmCommand(),
 	remove: new RemoveCommand(),
---
+++ commands/mobilefixtures.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const AsciiTable = require('ascii-table');
+
+const BaseCommand = require('./baseCommand.js');
+const firebase = require('../firebase.js');
+
+module.exports = class MobileFixturesCommand extends BaseCommand {
+	constructor() {
+		super('Viewfixtures', {
+			description: 'View all of the current unplayed fixtures',
+			args: {}
+		});
+	}
+
+	async execute(message, args) {
+		const fixtures = await firebase.getUnplayedMatches();
+		const players = await firebase.getLadder();
+
+		let table = new AsciiTable;
+		table.setHeading('Attacker', '-', 'Defender');
+
+		for (let fixture of fixtures) {
+			table.addRow(
+				`(${this.getPlayerPos(players, fixture.attacker)}) ${fixture.attacker.toUpperCase()}`,
+				'vs',
+				`(${this.getPlayerPos(players, fixture.defender)}) ${fixture.defender.toUpperCase()}`,
+			);
+		}
+
+		message.channel.send(`\`\`\`${table.toString()}\`\`\``);
+	}
+
+	getPlayerPos(players, name) {
+		const pos = players.filter(p => p.name === name)[0].position;
+
+		if (pos === -1) return 'NEW';
+		if (pos === 0) return 'KING';
+		return pos.toString();
+	}
+}
